b0294d99be9aea4edd9e362e8936587f0d80eccf
Joshua Martínez <djoshuamartinezpineda@gmail.com>
2019-04-09 18:33:51 -0500

    socket changes

--- api-implementations/sails-io-socket.js
+++ api-implementations/sails-io-socket.js
@@ -7,13 +7,14 @@ module.exports = {
         return new Promise((resolve, reject) => {
             if(configuration){
                 if(typeof(configuration.url)!=='string') {
-                    throw new TypeError("sails-io-socket url configuration is not a string.");
+                    reject(new TypeError("sails-io-socket url configuration is not a string."));
                 }
             }
             else{
-                throw new ReferenceError("sails-io-socket configuration wasn't supplied with a url.");
+                reject(new ReferenceError("sails-io-socket configuration wasn't supplied with a url."));
             }
-            const io = sailsIOClient(socketIOClient);
+            const io = sailsIOClient(socketIOClient/*, {pingTimeout: 3000}*/);
+            io.reconnection = true;
             io.sails.url = configuration.url;
 
             // Disables a method for getting cookie for the
@@ -21,12 +22,12 @@ module.exports = {
             // error on react native. It defaults to send a JSONP
             // request to the CORS enabled server.
             // (Not sure how they inject script tags here, though)
-            io.sails.userCORSRouteToGetCookie = false;
+            io.sails.useCORSRouteToGetCookie = false;
 
             // The sails socket autoconnects when instantiated,
             // any request is queued until connection is available,
             // so we can start our calls without fear.
-            io.socket[m](url, args, (data, jwr) => resolve({data, jwr}));
+            io.socket[method](url, args, (data, jwr) => resolve({data, jwr}));
         });
     }
 };
